4ad060209dea21ca517afc332850d5c47e84cf81
lucasboilly <boilly.lucas@gmail.com>
2020-01-28 01:06:46 +0100

    Amélioration du script JS

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 new fullpage('#fullpage', {
     //options here
-    scrollingSpeed: 1000,
+    scrollingSpeed: 800,
 	autoScrolling: true,
     scrollHorizontally: true,
     easing: 'easeInOutCubic',
@@ -11,33 +11,140 @@ new fullpage('#fullpage', {
 
 // 
 
-function doSomething() {
-    
-    anime.timeline({loop: false}).add({ targets: '#illustration2',
-        width: ['0', '100%'], duration: 1000, easing: "easeInOutExpo", delay: 0,
-        //delay: (el, i) => 50 * i
-    });
-
-
-    var textWrapper = document.querySelector('.project-title2 .letters');
-    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-    anime.timeline({loop: false}).add({ targets: '.project-title2 .letter',
-        translateY: ["1.1em", 0], opacity: [1, 1], duration: 1000, easing: "easeOutExpo", delay: 500, 
-        //delay: (el, i) => 50 * i
-    });
 /*
-    var textWrapper = document.querySelector('.project-subtitle2 .letters');
-    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-    anime.timeline({loop: false}).add({ targets: '.project-title2 .letter',
-        translateY: ["1.1em", 0], opacity: [1, 1], duration: 1000, easing: "easeOutExpo", delay: 500, 
-        //delay: (el, i) => 50 * i
-    });*/
 
-   
+inView('#s1').on('enter', i = 1);
+if i = 1 {
+    fuction animate section
+}
+*/
+
+console.log('ok');
+
+
+function animateSection1() {
+    
+    console.log("1");
+    let sectionNumber = 1;
+    let illustration = `#illustration${sectionNumber}`;
+    let title = `#title${sectionNumber}`;
+    let subtitle = `#subtitle${sectionNumber}`
+    let easing = "easeOutExpo";
+
+    anime.timeline({loop: false}).add({ 
+        targets: illustration,
+        scale: [0, 1],
+        duration: 2000, 
+        easing: easing
+    });
+
+    var textWrapper = document.querySelector(`${title} .letters`);
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    anime.timeline({loop: false}).add({ 
+        targets: `${title} .letter`,
+        translateY: ["1.1em", 0],
+        duration: 1000, 
+        easing: easing,
+        delay: 1000
+    });
+
+    var textWrapper = document.querySelector(`${subtitle} .letters`);
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    anime.timeline({loop: false}).add({ 
+        targets: `${subtitle} .letter`,
+        translateY: ["1.1em", 0],
+        duration: 1000, 
+        easing: easing,
+        delay: 1200
+        //delay: (el, i) => 50 * i
+    });
+}
+
+
+function animateSection2() {
+
+    console.log("2");
+    let sectionNumber = 2;
+    let illustration = `#illustration${sectionNumber}`;
+    let title = `#title${sectionNumber}`;
+    let subtitle = `#subtitle${sectionNumber}`
+    let easing = "easeOutExpo";
+
+    anime.timeline({loop: false}).add({ 
+        targets: illustration,
+        scale: [0, 1],
+        duration: 2000, 
+        easing: easing
+    });
+
+    var textWrapper = document.querySelector(`${title} .letters`);
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    anime.timeline({loop: false}).add({ 
+        targets: `${title} .letter`,
+        translateY: ["1.1em", 0],
+        duration: 1000, 
+        easing: easing,
+        delay: 1000
+    });
+
+    var textWrapper = document.querySelector(`${subtitle} .letters`);
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    anime.timeline({loop: false}).add({ 
+        targets: `${subtitle} .letter`,
+        translateY: ["1.1em", 0],
+        duration: 1000, 
+        easing: easing,
+        delay: 1200
+        //delay: (el, i) => 50 * i
+    });
+    
+}
+
+function animateSection3() {
+
+    console.log("3");
+    let sectionNumber = 3;
+    let illustration = `#illustration${sectionNumber}`;
+    let title = `#title${sectionNumber}`;
+    let subtitle = `#subtitle${sectionNumber}`
+    let easing = "easeOutExpo";
+
+    anime.timeline({loop: false}).add({ 
+        targets: illustration,
+        scale: [0, 1],
+        duration: 2000, 
+        easing: easing
+    });
+
+    var textWrapper = document.querySelector(`${title} .letters`);
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    anime.timeline({loop: false}).add({ 
+        targets: `${title} .letter`,
+        translateY: ["1.1em", 0],
+        duration: 1000, 
+        easing: easing,
+        delay: 1000
+    });
+
+    var textWrapper = document.querySelector(`${subtitle} .letters`);
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+    anime.timeline({loop: false}).add({ 
+        targets: `${subtitle} .letter`,
+        translateY: ["1.1em", 0],
+        duration: 1000, 
+        easing: easing,
+        delay: 1200
+        //delay: (el, i) => 50 * i
+    });
+    
 }
 
 
 
-//inView('#s1').on('enter', doSomething);
-inView('#s2').on('enter', doSomething);
-//inView('#s3').on('enter', doSomething);
+
+
+
+inView('#s1').on('enter', animateSection1);
+inView('#s2').on('enter', animateSection2);
+inView('#s3').on('enter', animateSection3);
+
