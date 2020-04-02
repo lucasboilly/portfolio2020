0a7ed0718bad81ea56cb686c9b692337d485e7b5
lucasboilly <boilly.lucas@gmail.com>
2020-04-02 16:48:26 +0200

    Correction des bugs

--- js/main.js
+++ js/main.js
@@ -14,8 +14,6 @@ new fullpage('#fullpage', {
 // Permet de règler le bug de la section 1 sur firefox
 inView.offset(1);
 
-
-
 function animateSection1() {
     
     console.log("1");
@@ -141,54 +139,8 @@ function animateSection3() {
         delay: 1200
         //delay: (el, i) => 50 * i
     });
-    
-}
-
-
-
-function animateSection4() {
-
-    console.log("4");
-    let sectionNumber = 4;
-    let illustration = `#illustration${sectionNumber}`;
-    let title = `#title${sectionNumber}`;
-    let subtitle = `#subtitle${sectionNumber}`
-    let easing = "easeOutExpo";
-
-    anime.timeline({loop: false}).add({ 
-        targets: illustration,
-        scale: [0, 1],
-        duration: 2000, 
-        opacity: [0, 1],
-        easing: easing
-    });
-
-    var textWrapper = document.querySelector(`${title} .letters`);
-    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-    anime.timeline({loop: false}).add({ 
-        targets: `${title} .letter`,
-        translateY: ["1.1em", 0],
-        duration: 1000, 
-        easing: easing,
-        opacity: [0, 1],
-        delay: 1000
-    });
-
-    var textWrapper = document.querySelector(`${subtitle} .letters`);
-    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-    anime.timeline({loop: false}).add({ 
-        targets: `${subtitle} .letter`,
-        translateY: ["1.1em", 0],
-        duration: 1000, 
-        easing: easing,
-        opacity: [0, 1],
-        delay: 1200
-        //delay: (el, i) => 50 * i
-    });
-    
 }
 
 inView('#s1').on('enter', animateSection1);
 inView('#s2').on('enter', animateSection2);
 inView('#s3').on('enter', animateSection3);
-inView('#s4').on('enter', animateSection4);
